533223ad43962525a505516a2234695f926f81ea
emenikenicholasatwork <nicholasemenikeatwork@gmail.com>
2024-08-15 07:22:47 +0100

    optimising my website

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let words = document.querySelectorAll(".word");
-words.forEach((word)=>{
+words.forEach((word) => {
     let letters = word.textContent.split("");
-    word.textContent="";
-    letters.forEach((letter)=>{
+    word.textContent = "";
+    letters.forEach((letter) => {
         let span = document.createElement("span");
         span.textContent = letter;
         span.className = "letter";
@@ -12,22 +12,22 @@ words.forEach((word)=>{
 
 
 let currentWordIndex = 0;
-let maxWordIndex = words.length -1;
+let maxWordIndex = words.length - 1;
 words[currentWordIndex].style.opacity = "1";
 
-let changeText = ()=> {
+let changeText = () => {
     let currentWord = words[currentWordIndex];
     let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
 
-    Array.from(currentWord.children).forEach((letter, i)=>{
-        setTimeout(()=>{
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
             letter.className = "letter out";
         }, i * 80);
     });
-    nextWord.style.opacity="1";
-    Array.from(nextWord.children).forEach((letter, i)=>{
-        letter.className="letter behind";
-        setTimeout(()=>{
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = "letter behind";
+        setTimeout(() => {
             letter.className = "letter in";
         }, 340 + i * 80);
     });
@@ -40,20 +40,20 @@ setInterval(changeText, 3000);
 
 //circle skill///////
 const circles = document.querySelectorAll('.circle');
-circles.forEach(elem=>{
+circles.forEach(elem => {
     var dots = elem.getAttribute("data-dots");
     var marked = elem.getAttribute("data-percent");
-    var percent = Math.floor(dots*marked/100);
+    var percent = Math.floor(dots * marked / 100);
     var points = "";
     var rotate = 360 / dots;
 
-    for(let i =0; i < dots; i++){
+    for (let i = 0; i < dots; i++) {
         points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
     }
     elem.innerHTML = points;
 
     const pointsMarked = elem.querySelectorAll('.points');
-    for(let i = 0; i<percent ; i++){
+    for (let i = 0; i < percent; i++) {
         pointsMarked[i].classList.add('marked')
     }
 })
@@ -68,11 +68,11 @@ var mixer = mixitup('.portfolio-gallery');
 let menuLi = document.querySelectorAll('header ul li a');
 let section = document.querySelectorAll('section');
 
-function activeMenu(){
+function activeMenu() {
     let len = section.length;
-    while(--len && window.scrollY + 97 < section[len].offsetTop){}
-        menuLi.forEach(sec => sec.classList.remove("active"));
-        menuLi[len].classList.add("active");
+    while (--len && window.scrollY + 97 < section[len].offsetTop) { }
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
 }
 
 activeMenu();
@@ -82,7 +82,7 @@ window.addEventListener("scroll", activeMenu);
 
 //sticky navbar
 const header = document.querySelector("header");
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.screenY > 50)
 })
 
@@ -90,33 +90,33 @@ window.addEventListener("scroll", function(){
 let menuIcon = document.querySelector("#menu-icon");
 let navlist = document.querySelector(".navlist");
 
-menuIcon.onclick = () =>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle("fa-x");
     navlist.classList.toggle("open");
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menuIcon.classList.remove("fa-x");
     navlist.classList.remove("open");
 }
 
 
 //parallax/////////
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show-items");
-        }else{
+        } else {
             entry.target.classList.remove("show-items");
         }
     });
 });
 
 const scrollScale = document.querySelectorAll(".scroll-scale");
-scrollScale.forEach((el)=>observer.observe(el));
+scrollScale.forEach((el) => observer.observe(el));
 
 const scrollBottom = document.querySelectorAll(".scroll-bottom");
-scrollBottom.forEach((el)=>observer.observe(el));
+scrollBottom.forEach((el) => observer.observe(el));
 
 const scrollTop = document.querySelectorAll(".scroll-top");
-scrollTop.forEach((el)=>observer.observe(el));
+scrollTop.forEach((el) => observer.observe(el));
